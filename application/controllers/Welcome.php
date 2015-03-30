63fc4d193b7f319567332db72f338af79cdc5919
RexBowen <rexbwen@outlook.com>
2015-03-29 17:01:03 -0700

    Lab7 in progress Signed-off-by:RexBowen <rexbwen@outlook.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -24,7 +24,9 @@ class Welcome extends Application {
         $files = array();
         foreach ($map as $str) {
             if (substr_compare($str, $test, strlen($str) - strlen($test), strlen($test)) === 0 && $str != 'menu.xml') {
-                $this->order->load($str);
+
+                //we don't need to load the entire orders in this case
+                $this->order->load($str, 1);
                 //remove the file extension, where 4 is the length of the extension
                 array_push($files, array('filename' => substr($str, 0, strlen($str) - 4),
                     'customer' => $this->order->getCustomer()
@@ -45,8 +47,35 @@ class Welcome extends Application {
         // Present the list to choose from
         $this->data['pagebody'] = 'justone';
         $this->data['filename'] = $filename;
-        $this->menu->patties();
+        $this->order->load($filename . '.xml', 2);
+        $this->data['customer'] = $this->order->getCustomer();
+        $this->data['type'] = $this->order->getType();
+        $burger_list = array();
+
+        $burgers = $this->order->burgers();
+        $count = 1;
+        foreach ($burgers as $burger) {
+            array_push($burger_list, array(
+                'name' => '*Burger #' . $count++ . '*',
+                'base' => $this->menu->getPatty($burger->patty),
+                'cheese' => ' '.$this->menu->getCheese($burger->top) . ' '.
+                $this->menu->getCheese($burger->bottom),
+                'sauces' => $this->__getSauce($burger)
+                
+            ));
+        }
+        $this->data['burgers'] = $burger_list;
         $this->render();
+    }
+    
+    private function __getSauce($burger){
+        if(count($burger->sauces)== 0) return '';
+        $result = 'Sauces: ';
+        foreach($burger->sauces as $sauce) {
+            $result.=$sauce;
+        }
+        
+        return $result;
     }
 
 }
